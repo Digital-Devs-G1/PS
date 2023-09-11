a3ed61a10363893d0734d8672fa0167844adfd05
Martin-Paez <mpaez.ok@gmail.com>
2023-09-11 15:58:17 -0300

    Inicie ReportDbContext, Termine VariableFields, Termine DataType

---
+++ Sistema-de-rendicion-de-gastos/Domain/Entities/VariableFields.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class VariableFields
+    {
+        public string Label { get; set; }
+        public string Value { get; set; }
+
+        public int DataType { get; set; }
+        public DataType DataTypeNav { get; set; }
+        public int ReportId { get; set; }
+        public Report ReportNav { get; set; }
+    }
+}
